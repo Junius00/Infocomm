33c689464b54cc225fc3b9721e713db019f2f580
Junius00 <junius.the.unknown@gmail.com>
2016-01-11 19:19:53 +0800

    Revert "Striker changes"
    This reverts commit 2bcedf996035e42ba4d429aef71458f431538252.

--- 2015/RoboCup/Programs/X1RCU/PRX1/HardwareInfo.c
+++ 2015/RoboCup/Programs/X1RCU/PRX1/HardwareInfo.c
@@ -14,47 +14,47 @@
 #define _COMPOUNDEYE3_ unsigned long
 #endif
 #ifndef _MOTOR_ne_
-#define _MOTOR_ne_ 152480
+#define _MOTOR_ne_ 138016
 #endif
 #ifndef _MOTOR_nw_
-#define _MOTOR_nw_ 135968
+#define _MOTOR_nw_ 154528
 #endif
 #ifndef _MOTOR_se_
-#define _MOTOR_se_ 253616
+#define _MOTOR_se_ 23584
 #endif
 #ifndef _MOTOR_sw_
-#define _MOTOR_sw_ 237104
+#define _MOTOR_sw_ 40096
 #endif
 #ifndef _SCABLEAD_f_
-#define _SCABLEAD_f_ 288
+#define _SCABLEAD_f_ 384
 #endif
 #ifndef _SCABLEAD_r_
-#define _SCABLEAD_r_ 416
+#define _SCABLEAD_r_ 128
 #endif
 #ifndef _SCABLEAD_b_
-#define _SCABLEAD_b_ 256
+#define _SCABLEAD_b_ 416
 #endif
 #ifndef _SCABLEAD_l_
-#define _SCABLEAD_l_ 384
+#define _SCABLEAD_l_ 160
 #endif
 #ifndef _COMPASS_1_
-#define _COMPASS_1_ 66304
+#define _COMPASS_1_ 82432
 #endif
 #ifndef _ADULTRASOUND_lus_
-#define _ADULTRASOUND_lus_ 32
+#define _ADULTRASOUND_lus_ 256
 #endif
 #ifndef _ADULTRASOUND_rus_
-#define _ADULTRASOUND_rus_ 0
+#define _ADULTRASOUND_rus_ 288
 #endif
 #ifndef _ADULTRASOUND_fus_
-#define _ADULTRASOUND_fus_ 128
+#define _ADULTRASOUND_fus_ 32
 #endif
 #ifndef _ADULTRASOUND_bus_
-#define _ADULTRASOUND_bus_ 160
+#define _ADULTRASOUND_bus_ 0
 #endif
 #ifndef _COMPOUNDEYE3_f_
-#define _COMPOUNDEYE3_f_ 69648
+#define _COMPOUNDEYE3_f_ 86560
 #endif
 #ifndef _COMPOUNDEYE3_b_
-#define _COMPOUNDEYE3_b_ 86160
+#define _COMPOUNDEYE3_b_ 18448
 #endif
